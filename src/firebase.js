e5929ced6d6b4994ab8b781ca990e0f0dc1d809e
WhiteFurr <farhanamujawar25@gmail.com>
2026-04-27 16:20:42 +0530

    docs: add repository link to readme

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPi1vwaAuYD01YZBU5xO2E7-OcD20cnaY",
-  authDomain: "careroute-77f0c.firebaseapp.com",
-  projectId: "careroute-77f0c",
-  storageBucket: "careroute-77f0c.firebasestorage.app",
-  messagingSenderId: "91647910264",
-  appId: "1:91647910264:web:3b915a242ef8031168288d",
-  measurementId: "G-2WY52NBT3B"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
